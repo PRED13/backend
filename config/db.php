6a762d81df2d53d54d003e8b2e7fd1d780faf6de
PRED13 <jm0824621@gmail.com>
2026-06-17 01:55:45 -0600

    ..

--- config/db.php
+++ config/db.php
@@ -1,6 +1,12 @@
 <?php
 // config/db.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
 // Obtenemos las variables desde Render
 $host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
